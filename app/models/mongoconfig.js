26ed40de74e3c2081ee6c4271b5d44d534e46119
Neil188 <19958604+Neil188@users.noreply.github.com>
2018-09-19 16:29:08 +0100

    dummy mongo config file

---
+++ app/models/mongoconfig.js
@@ -0,0 +1,10 @@
+// const config = {
+//     dbuser: "",
+//     dbpassword: "",
+//     mlabLink: "",
+// }
+
+// const dbUrl = `mongodb://${config.dbuser}:${config.dbpassword}@${config.mlabLink}`
+
+// module.exports = dbUrl;
+module.exports = undefined;
